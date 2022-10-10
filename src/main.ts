1acd6ef81bcb4b3381cc0eb9bc9de98533d06eea
= <=>
2022-10-10 13:33:25 -0400

    use environmental variables for redis set-up pre-bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const {
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_USERNAME,
+  REDIS_PASSWORD
+} = process.env;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: REDIS_HOST,
+      port: Number(REDIS_PORT),
+      username: REDIS_USERNAME,
+      password: REDIS_PASSWORD,
+    }
+  });
+  await app.listen();
 }
+
 bootstrap();
